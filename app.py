5f835c520e19daa8793d4441e6b9b324eb51e59d
Natalie Clark <nj_clark@hotmail.co.uk>
2023-10-06 18:22:32 +0100

    Add main program to print all users and all posts

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from lib.database_connection import DatabaseConnection
-# from lib.artist_repository import ArtistRepository
+from lib.user_repository import UserRepository
+from lib.post_repository import PostRepository
 
 
 # Connect to the database
@@ -7,12 +8,16 @@ connection = DatabaseConnection()
 connection.connect()
 
 # # Seed with some seed data
-# connection.seed("seeds/music_library.sql")
+# connection.seed("seeds/social_network.sql")
 
-# Retrieve all artists
-# artist_repository = ArtistRepository(connection)
-# artists = artist_repository.all()
+# Create repository objects
+user_repository = UserRepository(connection)
+post_repository = PostRepository(connection)
 
-# # List them out
-# for artist in artists:
-#     print(artist)
+# Print all users
+for user in user_repository.all():
+    print(user)
+
+# Print all posts
+for post in post_repository.all():
+    print(post)
